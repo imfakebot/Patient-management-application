e48302a77bf0aaa4d7177cbfe387c9b68fb991af
imfakebot <115884438+imfakebot@users.noreply.github.com>
2025-03-13 09:15:58 +0700

    add module sql and create DatabaseConnection Class

--- src/main/java/Repository/DatabaseConnection.java
+++ src/main/java/Repository/DatabaseConnection.java
@@ -1,5 +1,23 @@
 package Repository;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection {
+    private final String url = "jdbc:sqlserver://localhost:1433;databaseName=PMA;encrypt=true;trustServerCertificate=true;";
+    private final String user = "sa";
+    private final String password = "25012006anhahihi";
+
+    /**
+     * Establishes a connection to the SQL Server database using the provided URL,
+     * user, and password.
+     *
+     * @return A {@link Connection} object representing the connection to the
+     *         database.
+     * @throws SQLException If a database access error occurs.
+     */
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.pma {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.pma to javafx.fxml;
     exports com.pma;
