853abbc226535b304c0d40b25262de5b9a77ad44
imfakebot <115884438+imfakebot@users.noreply.github.com>
2025-03-15 20:37:45 +0700

    add javafx.graphi in module-info.java,update java-version and javafx version,fix code in login.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.pma {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires transitive javafx.graphics;
 
     opens com.pma to javafx.fxml;
+
     exports com.pma;
 }
