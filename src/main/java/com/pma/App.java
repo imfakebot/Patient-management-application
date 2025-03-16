2a9ed3a2be7088b785ba5d9d132a63445f9a231c
imfakebot <115884438+imfakebot@users.noreply.github.com>
2025-03-16 19:45:12 +0700

    test complete

--- src/main/java/com/pma/App.java
+++ src/main/java/com/pma/App.java
@@ -1,25 +1,11 @@
 package com.pma;
 
-import java.net.URL;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL fxmlUrl = getClass().getResource("/fxml/login.fxml");
-        if (fxmlUrl == null) {
-            throw new RuntimeException("FXML file not found!");
-        }
-        FXMLLoader loader = new FXMLLoader(fxmlUrl);
-        Parent root = loader.load();
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("JavaFX App");
-        primaryStage.show();
     }
 
     public static void main(String[] args) {
