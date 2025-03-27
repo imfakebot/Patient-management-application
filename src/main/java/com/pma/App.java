7d38e288de4daa92eaf3414ac833e90b34782cc1
imfakebot <115884438+imfakebot@users.noreply.github.com>
2025-03-27 21:54:31 +0700

    change in main

--- src/main/java/com/pma/App.java
+++ src/main/java/com/pma/App.java
@@ -1,15 +1,23 @@
 package com.pma;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        AnchorPane root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Login Form");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
